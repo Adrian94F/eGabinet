667962bb8eec12f48b82b87c99da0679419df89a
Adrian <209865@student.pwr.edu.pl>
2018-04-20 19:37:49 +0200

    init

---
+++ door_opener.js
@@ -0,0 +1,102 @@
+/* Oto postawiłem jako dar przed tobą drzwi otwarte, 
+których nikt nie może zamknąć, 
+bo ty chociaż moc masz znikomą, 
+zachowałeś moje słowo 
+i nie zaparłeś się mego imienia. 
+Ap 3,8 */
+
+function setCookie(cname, cvalue, exhours) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exhours * 60 * 60 * 1000));
+	var expires = "expires="+d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+function openTheDoor() {
+
+	var acc = getCookie("accessToken");
+	var ref = getCookie("refreshToken");
+
+	//alert(acc + " " + ref);
+
+	if (acc != "" && ref != "")
+	{
+		if (document.location.pathname.substring(location.pathname.lastIndexOf("/") + 1) == "login.html" /*document.location.href == "login.html"*/)
+		{
+			document.location.href = "index.html";
+		}
+	}
+	else
+		if (document.location.pathname.substring(location.pathname.lastIndexOf("/") + 1) != "login.html")
+		{
+			//alert("Zaloguj się!")
+			document.location.href = "login.html"
+		}
+
+}
+
+function login() {
+	// get username and password
+	var user = document.loginform.username.value;
+	var pass = document.loginform.password.value;
+	var data = {"login": user, "password": pass};
+	var host = "https://e-gabinet.org.pl:8181";
+
+	if (location.protocol != 'https:')
+		host = "http://e-gabinet.org.pl:8080";
+
+	// send request for token
+	$.ajax({
+		url: host + "/security/access",
+		type: "POST",
+		data: JSON.stringify(data),
+		contentType: "application/json",
+		dataType: "json",
+		success: function (response) {
+			//console.log(response);
+			var credentials = response;
+			setCookie("accessToken", response["accessToken"], 1);
+			setCookie("refreshToken", response["refreshToken"], 1);
+			//alert(getCookie("accessToken") + ":" + getCookie("refreshToken"));
+			document.location.href = "index.html";
+		},
+		error: function() {
+			$('.alert').show();
+			//alert("Niepoprawne dane logowania");
+		}
+	})
+}
+
+function logout() {
+	setCookie("accessToken", "", 0);
+	setCookie("refreshToken", "", 0);
+	document.location.href = "login.html";
+}
+
+// Check tokens
+openTheDoor();
+
+// Detect pressed CapsLock il login view
+document.addEventListener('keydown', function(event) {
+	var caps = event.getModifierState && event.getModifierState('CapsLock');
+	//console.log(caps);
+	if (document.location.pathname.substring(location.pathname.lastIndexOf("/") + 1) == "login.html" && caps)
+		$('#caps-alert').show();
+	else
+		$('#caps-alert').hide();
+});
---
+++ main.js
@@ -0,0 +1,58 @@
+//Create an account on Firebase, and use the credentials they give you in place of the following
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBngW2f2ty9lFFdHtzdDuRVLFBM8v16ZJk",
+    authDomain: "egabinet-1d5ab.firebaseapp.com",
+    databaseURL: "https://egabinet-1d5ab.firebaseio.com",
+    projectId: "egabinet-1d5ab",
+    storageBucket: "",
+    messagingSenderId: "107530078442"
+};
+firebase.initializeApp(config);
+
+
+var database = firebase.database().ref();
+var yourVideo = document.getElementById("yourVideo");
+var friendsVideo = document.getElementById("friendsVideo");
+var yourId = Math.floor(Math.random()*1000000000);
+var servers = {'iceServers': [{'urls': 'turn:numb.viagenie.ca','credential': 'pwrwekaw4','username': '210047@student.pwr.edu.pl'}]};
+var pc = new RTCPeerConnection(servers);
+pc.onicecandidate = (event => event.candidate?sendMessage(yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
+pc.onaddstream = (event => friendsVideo.srcObject = event.stream);
+
+function sendMessage(senderId, data) {
+    var msg = database.push({ sender: senderId, message: data });
+    msg.remove();
+}
+
+function readMessage(data) {
+    var msg = JSON.parse(data.val().message);
+    var sender = data.val().sender;
+    if (sender != yourId) {
+        if (msg.ice != undefined)
+            pc.addIceCandidate(new RTCIceCandidate(msg.ice));
+        else if (msg.sdp.type == "offer")
+            pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
+              .then(() => pc.createAnswer())
+              .then(answer => pc.setLocalDescription(answer))
+              .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})));
+        else if (msg.sdp.type == "answer")
+            pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+    }
+};
+
+database.on('child_added', readMessage);
+
+function showMyFace() {
+  navigator.mediaDevices.getUserMedia({audio:true, video:true})
+    .then(stream => yourVideo.srcObject = stream)
+    .then(stream => pc.addStream(stream));
+}
+
+function showFriendsFace() {
+  document.getElementById("loader").style.display = "none";
+  friendsVideo.style.display = "block";
+  pc.createOffer()
+    .then(offer => pc.setLocalDescription(offer) )
+    .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})) );
+}
